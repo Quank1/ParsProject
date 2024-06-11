a5d506cd160919e706f62ea374c192743274f0dc
Quank1 <rtmdlgj@gmail.com>
2024-06-11 10:35:20 +0300

    main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ price_list = []
 count = 0
 
 
-for num in range(1, 4):
+for num in range(1, 2):
 
 
     url = "https://www.olx.ua/uk/nedvizhimost/kvartiry/"
@@ -34,22 +34,25 @@ for num in range(1, 4):
     for i in data:
         sleep(1)
         try:
-            area = i.find("span", class_ = "css-643j0o").text.replace("м²", "")
+            try:
+                area = i.find("span", class_ = "css-643j0o").text.replace("м²", "")
 
-            if float(area) >= 50 and float(area) <= 60:
-                price_find = i.find("p", class_ = "css-tyui9s er34gjf0").text.replace("Договірна", "").replace("грн.", "").replace(" ", "")
+                if float(area) >= 50 and float(area) <= 60:
+                    price_find = i.find("p", class_ = "css-tyui9s er34gjf0").text.replace("Договірна", "").replace("грн.", "").replace(" ", "")
 
-                price_list.append(float(price_find))
-                count += 1
+                    price_list.append(float(price_find))
+                    count += 1
+
+            except ValueError:
+                pass
         except AttributeError:
-            print("Нету цены!")
+            pass
 
     url = next_page_url
 
 summa = sum(price_list) / count
 
 print(f"Средняя цена квартиры 50-60 м² - {int(summa)} грн")
-
 
 
 
